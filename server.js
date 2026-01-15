461ef256c000efd13b1508597b9a7eba24b3ea16
cozyleon00b-dev <cozyleon00b@gmail.com>
2026-01-16 06:45:37 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+
+const WEBHOOK_LOGS = "https://discord.com/api/webhooks/1461189182973018194/0hiq26Z3PnnceYnIgLXct8ZmVXlHSEW9dTbZgz-zZn7TwT9NFZdpMuOoEkcVo9viLWDQ";
+const WEBHOOK_NON_WL = "https://discord.com/api/webhooks/1461189186299105538/TN9SPHbuU1h3WfXcSFSR8iaXXx-uDOxzc-cfBpXHsTJv6o3BCYQg0FZxqRQNTC0z32VZ";
+
+let whitelistDB = [];
+
+// Roblox Check Endpoint
+app.get('/api/check', async (req, res) => {
+    const { id, user, map, maplink } = req.query;
+
+    if (whitelistDB.includes(id)) {
+        res.json({ allowed: true });
+    } else {
+        // Send to Discord
+        await axios.post(WEBHOOK_NON_WL, {
+            embeds: [{
+                title: "🚫 NON-WHITELIST KICKED",
+                color: 16711680,
+                fields: [
+                    { name: "Username", value: `[${user}](https://www.roblox.com/users/${id})` },
+                    { name: "Map Info", value: `[${map}](${maplink})` }
+                ],
+                footer: { text: "Security by VS CEO" }
+            }]
+        });
+        res.json({ allowed: false });
+    }
+});
+
+app.listen(3000, () => console.log("Vortex API Server Online"));
